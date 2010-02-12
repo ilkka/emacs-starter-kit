1ca1dd008d5b3f946a0417d83ebd43a7d320a231
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-02-12 11:47:21 +0200

    Missing(?) mode-compile added, loaded ido

--- init.el
+++ init.el
@@ -75,4 +75,15 @@
 (if (file-exists-p user-specific-dir)
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 
+(require 'ido)
+(ido-mode t)
+
+;;mode-compile
+(autoload 'mode-compile "mode-compile"
+ "Command to compile current buffer file based on the major mode" t)
+(global-set-key "\C-cc" 'mode-compile)
+(autoload 'mode-compile-kill "mode-compile"
+ "Command to kill a compilation launched by `mode-compile'" t)
+(global-set-key "\C-ck" 'mode-compile-kill)
+
 ;;; init.el ends here
