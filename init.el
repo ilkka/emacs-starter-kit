120d3ceb6f4c96a293e2b2c43ebb75401ac58f5e
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-15 16:57:48 +0200

    Automagically use YAML mode for .yml files

--- init.el
+++ init.el
@@ -103,6 +103,11 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/dict")
 (ac-config-default)
 
+;; yaml mode
+(add-to-list 'load-path "~/.emacs.d/yaml-mode")
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
 (require 'color-theme)
 (color-theme-zenburn)
 
